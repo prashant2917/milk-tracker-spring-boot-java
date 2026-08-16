bfc110ca02ce56cecc070b86fc8c18ace41741f1
Prashant Jondhale <jondhaleprashant@gmail.com>
2026-08-16 10:44:31 +0530

    Initial commit

---
+++ src/test/java/com/swarajya/milktracker/MilkTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swarajya.milktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilkTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
